b8fda09a674ce88088aeead52234870cb34cd7f3
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-17 21:07:35 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,111 @@
+//  console.log("Mensagem normal")
+// console.log("Mensagem normal");
+// console.info("Informação");
+// console.warn("Aviso");
+// console.error("Erro");
+
+//  console.table([
+//     {
+//       id: 1, tarefa: "EstudarJS"
+//     },
+//     {
+//       id: 2, tarefa: "Praticar codigo"
+//     }
+//  ])
+
+//  console.group("Grupo de logs")
+//  console.log('log 1')
+//  console.log('log 2')
+//  console.log('log 3')
+//  console.groupEnd()
+
+// console.time("Timer")
+// console.timeEnd("Timer")
+
+// let casa = ""
+// if(casa = "casa")
+//     console.log("casa")
+/* uwehjasdajsdnsadnsandsakdjsakdasjdksajkascnadndjsfndjndsfjsdfbsdjfsdjbsdfbjsdjfbsdfjsdfbjsdfbsdfbdsfbj */
+
+// var antigo = "Forma antiga de declarar variáveis"
+
+// let userName = "Maria"
+
+// const API_URL = "url"
+
+// let texto = "Ola"
+// console.log("nome = " + texto + " tipo:"+typeof (texto))
+
+// let numero = 42
+// console.log("nome = " + numero + " tipo:"+typeof (numero))
+
+// let isCompleted = false
+// console.log(isCompleted, typeof isCompleted);
+
+// let semValor;
+// console.log("semValor="+ semValor + " tipo:" + typeof (semValor));
+
+// let nulo = null
+// console.log("nulo="+ nulo + " tipo:"typeof (nulo));
+
+// let uniquedID = Symbol("id")
+// console.log(typeof (uniquedID));
+// console.log(uniquedID);
+
+// let bigNumero = 99999999999n
+// console.log("bigNumero="+ bigNumero + " tipo:" + typeof (bigNumero));
+
+// let task = {
+//     // Propriedade 1
+//   id: 1,
+//   // Propriedade 2
+//   title: "Aprender JavaScript",
+//   // Propriedade 3
+//   completed: false,
+//   // Propriedade 4
+//   assignee: {
+//     name: "João",
+//     email: "joao@example.com"
+//   },
+//   toggleComplete() {
+//     this.completed = !this.completed;
+//   }
+// };
+
+// let tasks = [
+    
+//   let task = {
+//   id: 1,
+//   title: "Aprender JavaScript",
+//   completed: false,
+//   assignee: {
+//     name: "João",
+//     email: "joao@example.com"
+//   },
+//   },
+// ]
+
+// let hoje = new Date()
+// console.log(hoje);
+
+// let amanha = new Date("03-18-2026")
+// console.log(amanha);
+
+// let pattern = /^\+55\(\d{2}\)\d{4,5}\-\d{4}$/;  
+// console.log(pattern.test("+55(11)92299-0899"));
+// console.log(pattern.test("+55(11)2299-0899"));
+
+let a = 10;
+let b = 3;
+console.log(a + b); // 13
+console.log(a - b); // 7
+console.log(a * b); // 30
+console.log(a / b); // 3.333...
+console.log(a % b); // 1
+console.log(a ** b); // 1000
+
+let contador = 0;
+
+console.log(++contador)
+console.log(contador++)
+
